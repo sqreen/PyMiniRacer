480f3d5876aea398eb956c56b4ba48ffbb9054a5
Boris FELD <lothiraldan@gmail.com>
2016-10-03 14:29:09 +0200

    Mark py_mini_racer as compatible with python3

--- setup.py
+++ setup.py
@@ -197,6 +197,10 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ],
     test_suite='tests',
     tests_require=test_requirements,
