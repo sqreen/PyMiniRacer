f7c44cbc264b1dc50425b0583648dc89c3e1c6cf
Boris FELD <lothiraldan@gmail.com>
2017-03-24 11:25:51 +0100

    Update dockerfile to run tests after building wheels

---
+++ docker/build_and_check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -x
+
+# Build the wheel
+python setup.py bdist_wheel
+
+# Create a virtualenv
+virtualenv /tmp/venv/
+
+# Install py_mini_racer
+/tmp/venv/bin/pip install dist/*.whl
+
+cd /tmp
+
+# Run basic tests
+/tmp/venv/bin/python -c "from py_mini_racer import py_mini_racer; mr = py_mini_racer.MiniRacer(); mr.eval(\"'a string'\")"
+
+# Run the tests
+/tmp/venv/bin/pip install -r /code/requirements/test.txt
+cp -Rv /code/tests /tmp/
+/tmp/venv/bin/py.test /tmp/tests
+
+ls /code/dist
