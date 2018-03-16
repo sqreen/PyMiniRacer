7ad4609549e402adadc60c61df7ff28473f39d7d
Sélim Menouar <slm.menouar@gmail.com>
2018-03-16 10:12:03 +0100

    Fix setup.py classifiers (#47)

--- setup.py
+++ setup.py
@@ -230,7 +230,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
