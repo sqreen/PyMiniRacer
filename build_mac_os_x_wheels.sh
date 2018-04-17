8190c6460f0081047e0f8967167d03b72c542f2c
Vivien Maisonneuve <vivien@sqreen.io>
2018-04-17 10:16:51 +0200

    Update pip in macOS build script

--- build_mac_os_x_wheels.sh
+++ build_mac_os_x_wheels.sh
@@ -4,6 +4,7 @@ set -x
 for PYVERSION in 2.7 3.4 3.5 3.6; do
     virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
     . ./venv_${PYVERSION}/bin/activate
+    curl https://bootstrap.pypa.io/get-pip.py | python
     pip install certifi
     python setup.py bdist_wheel
     deactivate
