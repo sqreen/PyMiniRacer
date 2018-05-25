c8966273f3b1d1d230f112dce0d23528f12508f9
Sélim <selim@sqreen.io>
2018-05-25 14:43:19 +0200

    Upload .tar.gz in s3

--- upload_artifacts_linux.sh
+++ upload_artifacts_linux.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 
 pip install --user awscli
-aws s3 cp dist/ "s3://sqreen-pyminiracer-travis-artefact/$(git rev-parse HEAD)/dist/" --recursive --exclude "*" --include "*.whl"
+aws s3 cp dist/ "s3://sqreen-pyminiracer-travis-artefact/$(git rev-parse HEAD)/dist/" --recursive --exclude "*" --include "*.whl" --include "*.tar.gz"
