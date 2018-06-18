d3703d0048f854c803981528047f53f2fb6f0d4f
Sélim <selim@sqreen.io>
2018-06-18 10:34:53 +0200

    Build wheel for Python 3.7

--- build_mac_os_x_wheels.sh
+++ build_mac_os_x_wheels.sh
@@ -1,7 +1,7 @@
 set -e
 set -x
 
-for PYVERSION in 2.7 3.4 3.5 3.6; do
+for PYVERSION in 2.7 3.4 3.5 3.6 3.7; do
     virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
     . ./venv_${PYVERSION}/bin/activate
     curl https://bootstrap.pypa.io/get-pip.py | python
