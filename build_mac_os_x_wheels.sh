dfb0b3e29ae53cd20da059a1138cceadf24a07ee
Taiki <Taiki-San@users.noreply.github.com>
2018-12-21 08:02:45 +0100

    Remove V8 debug symbols to improve build performance (#70)
    Cache the build artifacts of V8 to avoid having to recompile every time

--- build_mac_os_x_wheels.sh
+++ build_mac_os_x_wheels.sh
@@ -1,6 +1,8 @@
 set -e
 set -x
 
+PATH_TO_V8=$HOME/build/sqreen/PyMiniRacer/py_mini_racer/extension/v8/
+
 for PYVERSION in 2.7 3.4 3.5 3.6 3.7; do
     curl https://bootstrap.pypa.io/get-pip.py | python
     pip install virtualenv
@@ -11,7 +13,13 @@ for PYVERSION in 2.7 3.4 3.5 3.6 3.7; do
     . ./venv_${PYVERSION}/bin/activate
     pip install certifi
     pip install -r requirements/setup.txt
-    python setup.py build_v8
+
+    # Recyling V8
+    if [ ! -d $PATH_TO_V8 ] || [ -z "$(ls -A $PATH_TO_V8)" ]; then
+        rm -rf $PATH_TO_V8
+        python setup.py build_v8
+    fi
+    
     export LDSHARED="clang++ -bundle -undefined dynamic_lookup -arch i386 -arch x86_64"
     python setup.py bdist_wheel
     deactivate
