38ded4ce3d0973e9f1532f1c4e7119dfc450f2d9
Emile-Hugo SPIR <emile@sqreen.io>
2019-01-24 11:59:25 +0100

    Don't actually release a 0.1.19, because we're forced to introduce a new release channel for Alpine

--- build_linux_musl_wheels.sh
+++ build_linux_musl_wheels.sh
@@ -11,19 +11,19 @@ for PYVERSION in 2.7 3.4 3.5 3.6 3.7; do
 	CONT=$(date +%s)
 
 	docker run \
-	    -e PY_MINI_RACER_V8_PATH=_v8.so \
-	    -d \
-	    --name ${CONT} python:${PYVERSION}-alpine sh \
-	    -c "tail -f /dev/null"
+		-e PY_MINI_RACER_V8_PATH=_v8.so \
+		-d \
+		--name ${CONT} python:${PYVERSION}-alpine sh \
+		-c "tail -f /dev/null"
 
 	docker cp . ${CONT}:${BASE_PATH}
 	docker cp _v8.so ${CONT}:.
 
-    docker exec ${CONT} python setup.py sdist
-    docker exec ${CONT} python setup.py bdist_wheel
+	docker exec ${CONT} python setup.py sdist
+	docker exec ${CONT} python setup.py bdist_wheel
 
-    if [ PYVERSION == 3.7 ]; then
-    	docker exec ${CONT} sh -c "pip install auditwheel"
+	if [ PYVERSION == 3.7 ]; then
+		docker exec ${CONT} sh -c "pip install auditwheel"
 		docker exec ${CONT} sh -c "apk add patchelf"
 		docker exec ${CONT} sh -c "for i in $(ls dist/*.whl); do; auditwheel repair $i -w tmpdist; done"
 		docker exec ${CONT} sh -c "rm dist/*.whl"
@@ -33,7 +33,7 @@ for PYVERSION in 2.7 3.4 3.5 3.6 3.7; do
 		# Add wheel for Python without PyMalloc. Since we don't rely on it we can
 		# safely copy the wheel with another name
 		docker exec ${CONT} sh -c "python3 wheel_pymalloc.py"
-    fi
+	fi
 
 	docker exec ${CONT} tar cvzf wheels.tar.gz dist/
 	docker cp ${CONT}:wheels.tar.gz .
